753df624b39e3a3b58ed703f103c45ae7369663e
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-05-09 19:34:43 +0900

    [Fix] #4 Button 컴포넌트 disabled 속성 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -5,17 +5,19 @@ type ButtonProps = {
 	color?: string;
 	fontColor?: string;
 	onClick: () => void;
+	disabled?: boolean;
 };
 
-function Button({ text, color, fontColor, onClick }: ButtonProps) {
+function Button({ text, color, fontColor, onClick, disabled }: ButtonProps) {
 	return (
 		<div className={styles.container}>
 			<button
+				disabled={disabled}
 				onClick={onClick}
 				style={
 					{
-						"--button-color": color,
-						"--font-color": fontColor,
+						"--button-color": disabled ? "#EDF2F7" : color,
+						"--font-color": disabled ? "#A0AEC0" : fontColor,
 					} as React.CSSProperties
 				}
 			>
