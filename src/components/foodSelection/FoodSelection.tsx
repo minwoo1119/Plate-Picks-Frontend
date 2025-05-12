355a572d729eb114260380ac157f24e053c125f4
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-05-12 21:27:15 +0900

    [Feat] #5 음식 선호도 조사 컴포넌트 구현

---
+++ src/components/foodSelection/FoodSelection.tsx
@@ -0,0 +1,61 @@
+import { ThumbsDown, ThumbsUp, X } from "lucide-react";
+
+import { Preference } from "../../pages/progress/ProgressPage";
+import styles from "./foodSelection.module.scss";
+
+type FoodSelectionProps = {
+	id: number;
+	name: string;
+	description: string;
+	imgUrl: string;
+	preference: Preference;
+	setPreference: (pref: Preference) => void;
+};
+
+function FoodSelection({
+	preference,
+	setPreference,
+	imgUrl,
+	name,
+	description,
+}: FoodSelectionProps) {
+	return (
+		<div className={styles.container}>
+			<div className={styles.imgBox}>
+				<img src={imgUrl} alt="음식 이미지" />
+			</div>
+			<div className={styles.textBox}>
+				<div>{name}</div>
+				<div>{description}</div>
+			</div>
+			<div className={styles.preferenceBox}>
+				<div
+					className={`${styles.preferenceBtn} ${
+						preference === "like" ? styles.selected : ""
+					} ${styles.underline}`}
+					onClick={() => setPreference("like")}
+				>
+					<ThumbsUp width={15} />
+				</div>
+				<div
+					className={`${styles.preferenceBtn} ${
+						preference === "dislike" ? styles.selected : ""
+					} ${styles.underline}`}
+					onClick={() => setPreference("dislike")}
+				>
+					<ThumbsDown width={15} />
+				</div>
+				<div
+					className={`${styles.preferenceBtn} ${
+						preference === "none" ? styles.selected : ""
+					}`}
+					onClick={() => setPreference("none")}
+				>
+					<X width={15} />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default FoodSelection;
