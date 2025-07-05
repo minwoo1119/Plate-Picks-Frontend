d0df8215fee8926d37f56f5079c99f4d2f5e3c76
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-05 16:55:11 +0900

    [Fix] #16 - 백엔드와 동일한 enum 형식으로 수정

--- src/components/foodSelection/FoodSelection.tsx
+++ src/components/foodSelection/FoodSelection.tsx
@@ -1,61 +1,61 @@
-import { ThumbsDown, ThumbsUp, X } from "lucide-react";
+import { ThumbsDown, ThumbsUp, X } from 'lucide-react';
 
-import { Preference } from "../../pages/survey/SurveyPage";
-import styles from "./foodSelection.module.scss";
+import { Preference } from '../../pages/survey/SurveyPage';
+import styles from './foodSelection.module.scss';
 
 type FoodSelectionProps = {
-	id: number;
-	name: string;
-	description: string;
-	imgUrl: string;
-	preference: Preference;
-	setPreference: (pref: Preference) => void;
+    id: number;
+    name: string;
+    description: string;
+    imgUrl: string;
+    preference: Preference;
+    setPreference: (pref: Preference) => void;
 };
 
 function FoodSelection({
-	preference,
-	setPreference,
-	imgUrl,
-	name,
-	description,
+    preference,
+    setPreference,
+    imgUrl,
+    name,
+    description,
 }: FoodSelectionProps) {
-	return (
-		<div className={styles.container}>
-			<div className={styles.imgBox}>
-				<img src={imgUrl} alt="음식 이미지" />
-			</div>
-			<div className={styles.textBox}>
-				<div>{name}</div>
-				<div>{description}</div>
-			</div>
-			<div className={styles.preferenceBox}>
-				<div
-					className={`${styles.preferenceBtn} ${
-						preference === "like" ? styles.selected : ""
-					} ${styles.underline}`}
-					onClick={() => setPreference("like")}
-				>
-					<ThumbsUp width={15} />
-				</div>
-				<div
-					className={`${styles.preferenceBtn} ${
-						preference === "dislike" ? styles.selected : ""
-					} ${styles.underline}`}
-					onClick={() => setPreference("dislike")}
-				>
-					<ThumbsDown width={15} />
-				</div>
-				<div
-					className={`${styles.preferenceBtn} ${
-						preference === "none" ? styles.selected : ""
-					}`}
-					onClick={() => setPreference("none")}
-				>
-					<X width={15} />
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className={styles.container}>
+            <div className={styles.imgBox}>
+                <img src={imgUrl} alt='음식 이미지' />
+            </div>
+            <div className={styles.textBox}>
+                <div>{name}</div>
+                <div>{description}</div>
+            </div>
+            <div className={styles.preferenceBox}>
+                <div
+                    className={`${styles.preferenceBtn} ${
+                        preference === 'Good' ? styles.selected : ''
+                    } ${styles.underline}`}
+                    onClick={() => setPreference('Good')}
+                >
+                    <ThumbsUp width={15} />
+                </div>
+                <div
+                    className={`${styles.preferenceBtn} ${
+                        preference === 'Bad' ? styles.selected : ''
+                    } ${styles.underline}`}
+                    onClick={() => setPreference('Bad')}
+                >
+                    <ThumbsDown width={15} />
+                </div>
+                {/* <div
+                    className={`${styles.preferenceBtn} ${
+                        preference === 'Soso' ? styles.selected : ''
+                    }`}
+                    onClick={() => setPreference('Soso')}
+                >
+                    <X width={15} />
+                </div> */}
+            </div>
+        </div>
+    );
 }
 
 export default FoodSelection;
