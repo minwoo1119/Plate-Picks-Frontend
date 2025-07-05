979022c43f4d277eff2428f97648e0e4e561abd8
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-05 16:29:49 +0900

    [Feat] #16 - 모임 참여 api 연결

--- src/pages/join/JoinPage.tsx
+++ src/pages/join/JoinPage.tsx
@@ -1,43 +1,68 @@
-import Button from "../../components/common/Button";
-import Header from "../../components/common/Header";
-import Input from "../../components/common/Input";
-import { Users } from "lucide-react";
-import styles from "./joinPage.module.scss";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import Button from '../../components/common/Button';
+import Header from '../../components/common/Header';
+import Input from '../../components/common/Input';
+import { Users } from 'lucide-react';
+import axios from 'axios';
+import styles from './joinPage.module.scss';
 
 function JoinPage() {
-	const [title, setTitle] = useState("default title");
-	const [partyId, setPartyId] = useState("ajksn12knd");
-	const [name, setName] = useState("");
-	const navigate = useNavigate();
-	return (
-		<div className={styles.container}>
-			<Header text="모임 참여" />
-			<div className={styles.content}>
-				<Users />
-				<div className={styles.titleText}>{title}</div>
-				<div className={styles.groupId}>모임 ID : {partyId}</div>
-				<div className={styles.description}>
-					이름을 입력한 후 메뉴 선정을 위해 선호도를 입력해주세요. 모든 참가자의
-					선호도를 바탕으로 최적의 메뉴를 추천해드립니다.
-				</div>
-				<Input
-					label="이름"
-					onChange={(e) => setName(e.target.value)}
-					placeholder="이름을 입력하세요."
-					type="text"
-				/>
-			</div>
-			<Button
-				text="응답 시작"
-				color="#5046E5"
-				fontColor="white"
-				onClick={() => navigate("/survey", { state: { name } })}
-				disabled={name.length === 0}
-			/>
-		</div>
-	);
+    // 방 이름 정할 수 있게 추후에 수정
+    const [title, setTitle] = useState('default title');
+    const [partyId, setPartyId] = useState('ajksn12knd');
+    const [name, setName] = useState('');
+    const location = useLocation();
+    const navigate = useNavigate();
+    const roomCode = location.state.code;
+    const handleBtnClick = async () => {
+        try {
+            const response = await axios.post(
+                'http://119.56.230.161:7777/participants',
+                {
+                    name: name,
+                    roomCode: roomCode,
+                },
+            );
+            console.log('userID : ', response.data.id);
+
+            navigate('/survey', { state: { name, id: response.data.id } });
+        } catch (err) {
+            console.error('참가 중 오류 발생', err);
+        }
+    };
+    useEffect(() => {
+        setPartyId(roomCode);
+    }, []);
+    return (
+        <div className={styles.container}>
+            <Header text='모임 참여' />
+            <div className={styles.content}>
+                <Users />
+                <div className={styles.titleText}>{title}</div>
+                <div className={styles.groupId}>모임 ID : {partyId}</div>
+                <div className={styles.description}>
+                    이름을 입력한 후 메뉴 선정을 위해 선호도를 입력해주세요.
+                    모든 참가자의 선호도를 바탕으로 최적의 메뉴를
+                    추천해드립니다.
+                </div>
+                <Input
+                    label='이름'
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder='이름을 입력하세요.'
+                    type='text'
+                />
+            </div>
+            <Button
+                text='응답 시작'
+                color='#5046E5'
+                fontColor='white'
+                onClick={() => handleBtnClick()}
+                disabled={name.length === 0}
+            />
+        </div>
+    );
 }
 
 export default JoinPage;
